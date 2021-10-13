66ddd2c11a0bc8552631ee8f997d3d909b0cde84
ackermanjayjay <reza23198@gmail.com>
2021-10-13 17:04:42 +0700

    Nostalgia with C

---
+++ C/Pointers C/tes1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int* p,c;
+    printf("Masukkan angka yang di pointer: ");
+    scanf("%d",&c);
+
+    printf("Hasil alamat anda: %p",c);
+    printf("\nHasil angka anda: %d",c);
+}
